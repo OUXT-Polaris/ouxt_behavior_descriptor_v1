d66781808df67e59ca53e2de443ddfe354fed305
HansRobo <pythagora.yoshimoto@gmail.com>
2021-04-29 12:21:54 +0900

    Fix code check

--- include/ouxt_behavior_descriptor_v1/data_structures.hpp
+++ include/ouxt_behavior_descriptor_v1/data_structures.hpp
@@ -1,33 +1,60 @@
-template <typename T> struct BlackBoard {
+// Copyright (c) 2021, OUXT-Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OUXT_BEHAVIOR_DESCRIPTOR_V1__DATA_STRUCTURES_HPP_
+#define OUXT_BEHAVIOR_DESCRIPTOR_V1__DATA_STRUCTURES_HPP_
+
+#include <string>
+#include <vector>
+
+template<typename T>
+struct BlackBoard
+{
   std::string input;
   std::string eval;
 };
 
-struct Behavior {
+struct Behavior
+{
   std::string description;
   std::vector<BlackBoard> blackboard;
 };
-struct Position {
+struct Position
+{
   double x, y, z;
 };
-struct Quaternion {
+struct Quaternion
+{
   double x, y, z, w;
 };
-struct Pose {
+struct Pose
+{
   Position position;
   Quaternion orientation;
 };
 
-struct Object {
+struct Object
+{
   int uuid;
   std::vector<std::string> attributes;
   Pose pose;
 };
 
 
-struct Format{
+struct Format
+{
   Behavior behavior;
   std::vector<Object> objects;
 };
-
-
+#endif  // OUXT_BEHAVIOR_DESCRIPTOR_V1__DATA_STRUCTURES_HPP_
--- include/ouxt_behavior_descriptor_v1/operators.hpp
+++ include/ouxt_behavior_descriptor_v1/operators.hpp
@@ -1,19 +1,28 @@
+// Copyright (c) 2021, OUXT-Polaris
 //
-// Created by hans on 2021/04/29.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#ifndef OUXT_BEHAVIOR_DESCRIPTOR_V1_OPERATORS_HPP
-#define OUXT_BEHAVIOR_DESCRIPTOR_V1_OPERATORS_HPP
-
+#ifndef OUXT_BEHAVIOR_DESCRIPTOR_V1__OPERATORS_HPP_
+#define OUXT_BEHAVIOR_DESCRIPTOR_V1__OPERATORS_HPP_
 
 void operator(const YAML::Node &node, Format &format) {
-node["behavior"] >> Format.behavior;
-const YAML::Node& objects_node = node["objects"];
-for(auto object _node: objects_node){
-Object object;
-object_node >> object;
-format.objects.push_back(object);
-}
+  node["behavior"] >> Format.behavior;
+  const YAML::Node &objects_node = node["objects"];
+  for (auto object _node : objects_node) {
+    Object object;
+    object_node >> object;
+    format.objects.push_back(object);
+  }
 }
 
 void operator>>(const YAML::Node &node, Object &object) {
@@ -30,31 +39,31 @@ void operator>>(const YAML::Node &node, Pose &pose) {
   node["position"] >> pose.position;
   node["orientation"] >> pose.orientation;
 }
-void operator>>(const YAML::Node &node,Position &position) {
+void operator>>(const YAML::Node &node, Position &position) {
   node["x"] >> position.x;
   node["y"] >> position.y;
   node["z"] >> position.z;
 }
-void operator>>(const YAML::Node &node,Quaternion &quaternion) {
+void operator>>(const YAML::Node &node, Quaternion &quaternion) {
   node["x"] >> quaternion.x;
   node["y"] >> quaternion.y;
   node["z"] >> quaternion.z;
   node["w"] >> quaternion.w;
 }
 
-void operator>>(const YAML::Node &node,Behavior &behavior) {
-  node["description"] >>behavior.description;
+void operator>>(const YAML::Node &node, Behavior &behavior) {
+  node["description"] >> behavior.description;
   const YAML::Node &blackboards_node = node["blackboard"];
-  for (auto blackboard_node : blackboards_node){
+  for (auto blackboard_node : blackboards_node) {
     BlackBoard blackboard;
     blackboard_node >> blackboard;
     behavior.blackboard.push_back(blackboard);
   }
 }
 
-void operator>>(const YAML::Node &node,BlackBoard &blackBoard) {
+void operator>>(const YAML::Node &node, BlackBoard &blackBoard) {
   node["input"] >> blackboard.input;
   node["eval"] >> blackboard.eval;
 }
 
-#endif // OUXT_BEHAVIOR_DESCRIPTOR_V1_OPERATORS_HPP
+#endif  // OUXT_BEHAVIOR_DESCRIPTOR_V1__OPERATORS_HPP_
